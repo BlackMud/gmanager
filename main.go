c0319cf4e63bc087ec8484830ac58e510fc2b7c2
BlackMud <13526539736@139.com>
2018-03-17 11:46:45 +0800

    set ssh config stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"golang.org/x/crypto/ssh"
 	"log"
 	"os"
 )
@@ -13,6 +14,7 @@ var (
 	op_type = flag.String("m", "", "input the module which you will chose and do sth...")
 	src_fd  = flag.String("src", "", "input source file")
 	dst_fd  = flag.String("dst", "", "input dest file or directory")
+	config  = &ssh.ClientConfig{User: "root", Auth: []ssh.AuthMethod{ssh.Password("root12300.")}}
 )
 
 //复制文件或目录到目标主机上
@@ -39,11 +41,43 @@ func main() {
 		log.Fatal("i and m 不能为空")
 	}
 
-	ok, _ := PathExist(*src_fd)
-	if !ok {
-		log.Fatal("file or directory is not exists")
+	//if ok, _ := PathExist(*src_fd); !ok {
+	//	log.Fatal("file or directory is not exists")
+	//}
+
+	//处理连接过程中的信息
+	ce := func(err error, msg string) {
+		if err != nil {
+			log.Fatalf("%s error: %v", msg, err)
+		}
 	}
 
-	fmt.Printf("src file: %s\n", *src_fd)
-	fmt.Println(flag.Args())
+	//请求建立链接
+	client, err := ssh.Dial("tcp", *ip, config)
+	ce(err, "Dial")
+
+	//创建连接回话
+	session, err := client.NewSession()
+	ce(err, "new session")
+	defer session.Close()
+
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	session.Stdin = os.Stdin
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,
+		ssh.TTY_OP_ISPEED: 14400,
+		ssh.TTY_OP_OSPEED: 14400,
+	}
+
+	err = session.RequestPty("xterm", 25, 80, modes)
+	ce(err, "request pty")
+
+	err = session.Shell()
+	ce(err, "start shell")
+
+	err = session.Wait()
+	ce(err, "return")
+
 }
